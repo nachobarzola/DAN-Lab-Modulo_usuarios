730455dd5742685ec37369a4113ca3f896de9a82
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-03-27 19:30:01 -0300

    nachobarzola
    Se agregaron getters y setters de la calses tipoObra

--- src/main/java/dan/ms/usuarios/domain/TipoObra.java
+++ src/main/java/dan/ms/usuarios/domain/TipoObra.java
@@ -3,4 +3,32 @@ package dan.ms.usuarios.domain;
 public class TipoObra {
 	private Integer id;
 	private String descripcion;
+	
+	
+	public TipoObra(Integer id, String descripcion) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+	}
+	public TipoObra() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	@Override
+	public String toString() {
+		return "TipoObra [id=" + id + ", descripcion=" + descripcion + "]";
+	}
+	
+	
 }
