271c64aa699523d77331bc8fea57ff576d00ed9b
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-03-27 19:59:44 -0300

    nachobarzola
    Se agregaron getters y setters a clases faltantes

--- src/main/java/dan/ms/usuarios/domain/TipoUsuario.java
+++ src/main/java/dan/ms/usuarios/domain/TipoUsuario.java
@@ -4,4 +4,36 @@ public class TipoUsuario {
 	private Integer id;
 	private String tipo;
 	
+	public TipoUsuario(Integer id, String tipo) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+	}
+
+	public TipoUsuario() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoUsuario [id=" + id + ", tipo=" + tipo + "]";
+	}
+	
+	
 }
--- src/main/java/dan/ms/usuarios/domain/Usuario.java
+++ src/main/java/dan/ms/usuarios/domain/Usuario.java
@@ -6,5 +6,56 @@ public class Usuario {
 	private String password;
 	private TipoUsuario tipoUsuario;
 	
+	public Usuario(Integer id, String user, String password, TipoUsuario tipoUsuario) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.password = password;
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public Usuario() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public TipoUsuario getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", user=" + user + ", password=" + password + ", tipoUsuario=" + tipoUsuario + "]";
+	}
+	
+	
+	
 
 }
