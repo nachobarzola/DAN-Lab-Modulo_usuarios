5ec698a0311b86bf9cb2f2d5e5d607267c4dcbf7
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-03-27 13:20:49 -0300

    nachobarzola
    Se agrego en EmpleadoRest el metodo para actualizar empleados

--- src/main/java/dan/ms/usuarios/rest/EmpleadoRest.java
+++ src/main/java/dan/ms/usuarios/rest/EmpleadoRest.java
@@ -2,9 +2,13 @@ package dan.ms.usuarios.rest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dan.ms.usuarios.domain.Empleado;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 
 @RestController
@@ -28,6 +35,27 @@ public class EmpleadoRest {
 		nuevoEmp.setId(ID_GEN++);
 		listaEmpleado.add(nuevoEmp);
 		return ResponseEntity.ok(nuevoEmp);
+	}
+	
+	@PutMapping(path = "/{id}")
+	 @ApiOperation(value = "Actualiza un empleado")
+	@ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Actualizado correctamente"),
+    @ApiResponse(code = 401, message = "No autorizado"),
+    @ApiResponse(code = 403, message = "Prohibido"),
+    @ApiResponse(code = 404, message = "El ID no existe")
+	})
+	public ResponseEntity<Empleado> actualizar(@RequestBody Empleado empleado, @PathVariable Integer id){
+		OptionalInt index = IntStream.range(0,listaEmpleado.size())
+				.filter(i -> listaEmpleado.get(i).getId().equals(id))
+				.findFirst();
+		if(index.isPresent()) {
+			listaEmpleado.set(index.getAsInt(),empleado);
+			return ResponseEntity.ok(empleado);
+		}
+		else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	
