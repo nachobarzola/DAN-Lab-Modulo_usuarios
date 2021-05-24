7015c8a81585fd26b3a07d5ab8ebc2d5f51a4d9f
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-05-24 17:34:46 -0300

    Se actualizo un metodo

--- src/main/java/dan/ms/usuarios/services/PedidoRestExternoServiceImp.java
+++ src/main/java/dan/ms/usuarios/services/PedidoRestExternoServiceImp.java
@@ -1,30 +1,50 @@
 package dan.ms.usuarios.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import dan.ms.usuarios.domain.Obra;
+import dan.ms.usuarios.services.interfaces.ObraService;
 import dan.ms.usuarios.services.interfaces.PedidoRestExternoService;
 
 @Service
 public class PedidoRestExternoServiceImp implements PedidoRestExternoService{
 
-	private static String API_REST_PEDIDO = "http://localhost:9002/api";
-	private static String ENDPOINT_PEDIDO = "/pedido";
+	private static String API_REST_PEDIDO = "http://localhost:9002/";
+	private static String ENDPOINT_PEDIDO = "api/pedido";
+	
+	@Autowired
+	ObraService obraService;
+	
 	
 	@Override
 	public Boolean tienePedidos(Integer idCliente) {
 		RestTemplate restUsuario = new RestTemplate();
 		String uri = API_REST_PEDIDO + ENDPOINT_PEDIDO;
+		//
+		ResponseEntity<Object[]> respuesta;
+		Object[] pedidosRespuesta;
+		Boolean tienePedidos;
+		//
+		List<Obra> listaObra = obraService.buscarObraPorIdCliente(idCliente);
+		
+		//Chequemos si tiene pedidos cada obra de un cliente
+		for(Obra unaObra: listaObra) {
+			uri = uri+ "/obra/" + unaObra.getId();
+			respuesta = restUsuario.exchange(uri, HttpMethod.GET, null, Object[].class);
+			pedidosRespuesta = respuesta.getBody();
 
-		uri = uri + "?idCliente=" + idCliente;
-
-		ResponseEntity<Object[]> respuesta = restUsuario.exchange(uri, HttpMethod.GET, null, Object[].class);
-		Object[] pedidosRespuesta = respuesta.getBody();
-
-		Boolean tienePedidos = (pedidosRespuesta.length > 0);
-		return tienePedidos;
+			tienePedidos = (pedidosRespuesta.length > 0);
+			if(tienePedidos) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
